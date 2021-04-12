7e22bafb8d8657c660d6b091980b963d55247e4f
108062108 <ruby20010203@gmail.com>
2021-04-12 20:53:01 +0800

    50%

--- public/js/chatroom.js
+++ public/js/chatroom.js
@@ -1,8 +1,28 @@
+let cur_room;
+
 function init() {
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
-      document.getElementById("name").innerHTML = user.displayName;
-      document.getElementById("email").innerHTML = user.email;
+      /*$("#roombtn").click(function(){ 
+        console.log("room click");
+        chooseChatroom(n); 
+      });*/
+
+      var name = $("#name");
+      var email = $("#email");
+      name.html(user.displayName);
+      email.html(user.email);
+      
+      var mychatroom = $("#mychatroom");
+      firebase.database().ref('users/' + name.html()).once('value', snapshot => {
+        for(var n in snapshot.val()){
+          if(n != "email" && n != "password") {
+            mychatroom.append('<button onclick="chooseChatroom(\''+n+'\');">'+ n +'</button><br>');
+            //mychatroom.append('<button id="roombtn">' + n +'</button><br>');
+          }
+            //mychatroom.append(`<button onclick="chooseChatroom(${n});">`+ n +'</button><br>');
+        }
+      });
     } else {
         // No user is signed in.
     }
@@ -10,5 +30,64 @@ function init() {
 }
 
 function createChatroom() {
+  var room_name = prompt("Chatroom Name", "");
+  var name = $("#name");
 
+  var mychatroom = $("#mychatroom");
+  firebase.database().ref('users/' + name.html() + "/" + room_name).set({
+    room_name: room_name,
+  });
+
+  firebase.database().ref('content/' + room_name).set({
+    start: 0,
+  });
+
+  mychatroom.append('<button id="roombtn">' + room_name +'</button><br>');
+  //$("#roombtn").addEventListener("click", function(){ chooseChatroom(room_name); });
+}
+
+function chooseChatroom(n) {
+  cur_room = n;
+  console.log(n);
+  var database = firebase.database().ref('content/' + n);
+  var content = $("#content");
+  var roomname = $("#roomname");
+  roomname.html(cur_room);
+  database.on('value', snapshot => {
+    content.html("");
+    for(var i in snapshot.val()){
+      if(i != "start")
+        content.append('<span>'+snapshot.val()[i].name+':'+snapshot.val()[i].message+'</span><br>');
+    }
+  });
+}
+
+function addNewMember() {
+  var new_email = prompt("Email", "");
+  var database = firebase.database().ref('users/');
+  database.once('value', snapshot => {
+    for(var n in snapshot.val()) {
+      if(snapshot.child(n).child("email").val() == new_email) {
+        console.log(n);
+        firebase.database().ref('users/' + n + "/" + cur_room).set({
+          room_name: cur_room,
+        });
+        break;
+      }
+    }
+  }).then(() => { 
+    alert("Add Successfully!");
+  });
+}
+
+function sendMessage() {
+  var name = $("#name");
+  var message = $("#message");
+  console.log(cur_room);
+  var database = firebase.database().ref('content/' + cur_room);
+  database.push({
+    name: name.html(),
+    message : message.val()
+  });
+  message.val("");
 }
--- public/js/sign.js
+++ public/js/sign.js
@@ -1,15 +1,15 @@
 function writeUserData() {
-  var name = document.getElementById("name").value;
-  var email = document.getElementById("email").value;
-  var password = document.getElementById("password").value;
+  var name = $("#name");
+  var email = $("#email");
+  var password = $("#password");
 
-  firebase.auth().createUserWithEmailAndPassword(email,password).then(() => {
+  firebase.auth().createUserWithEmailAndPassword(email.val(), password.val()).then(() => {
     firebase.auth().currentUser.updateProfile({
-      displayName: name
+      displayName: name.val()
     });
-    firebase.database().ref('users/' + name).set({
-      email: email,
-      password : password
+    firebase.database().ref('users/' + name.val()).set({
+      email: email.val(),
+      password: password.val()
     }).then(() => { 
       alert("Successfully Signed Up!");
       window.location.href = "./sign_in.html";
@@ -22,10 +22,10 @@ function writeUserData() {
 }
 
 function SignIn() {
-  var email = document.getElementById("email").value;
-  var password = document.getElementById("password").value;
+  var email = $("#email");
+  var password = $("#password");
 
-  firebase.auth().signInWithEmailAndPassword(email, password).then(() => {
+  firebase.auth().signInWithEmailAndPassword(email.val(), password.val()).then(() => {
     alert("Successfully Signed In!");
     window.location.href = "./index.html";
   }).catch(error => { 
